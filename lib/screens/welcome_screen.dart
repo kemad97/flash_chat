02cdd97c749257fc9df73b8fd77da7f22a7876fc
Kerolos Emad <90790284+kemad97@users.noreply.github.com>
2025-08-15 17:56:50 +0300

    add animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,14 +2,58 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'registration_screen.dart';
 
+
 class WelcomeScreen extends StatefulWidget {
   static  const String id = 'welcome_screen';
+
 
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+
+   late AnimationController controller;
+    late Animation animation;
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    controller.forward();
+
+     animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.decelerate,
+    );
+    animation.addStatusListener ((status){
+      if(status == AnimationStatus.completed){
+        controller.reverse(from: 1.0);
+      }
+      else if(status == AnimationStatus.dismissed){
+        controller.forward();
+      }
+    }
+    );
+    controller.addListener(
+      () {
+        setState(() {
+          // This will rebuild the widget tree whenever the animation value changes.
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +70,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag:'logo' ,
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: animation.value *80.0,
                   ),
                 ),
                 Text(
@@ -34,6 +78,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
+                    color: Colors.black
                   ),
                 ),
               ],
